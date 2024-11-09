97ed196ac42f658d3a4cff45e3099a844f2e74bf
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2024-11-10 00:09:08 +0530

    updated input.jsx

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,40 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Input = () => {
-    const [val,setVal]=useState('');
+    const [val,setVal]=useState(0);
     const [cards,setCards]=useState([{}]);
+    var cnt=0;
+    var [cardStatus,setCardStatus]=useState([]);
     var obj={};
-    const createCards=(e)=>{
-        e.preventDefault();
-        setVal(e.target.value);
-        console.log(arr)
+    
+    
+    const createCards=()=>{
+        // e.preventDefault();
+        
+
+        if(val==''){ //if the size is 0
+            setCards([{}]);
+            return;
+        }
+        [...Array(val)].map((items,i)=>{
+            setCards(prevCards => [
+                ...prevCards,
+                { id: i, status: true },
+                { id: i, status: true }
+            ]);
+        }) 
+            
+    
+        console.log(cards,val);
+        
+
     }
+
+    useEffect(()=>{
+        createCards();
+    },[val])
+    
+    
     return (
         <>
             <h1 className='text-center'>
-                <label for="inputname" className="block py-5 text-gray-800 font-semibold text-md">
+                <span  className="block py-5 text-gray-800 font-semibold text-md">
                     Enter The Size (Even No's only)
-                </label>
+                </span>
                 <h1 className="">
                     <input
                         type="text"
-                        name="usrVal"
                         className=" text-center w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
-                        onChange={createCards}
+                        onChange={(e)=>setVal(e.target.value)}
                         
                     />
-                    {/* <p>{val}</p> */}
+                    <p>{val}</p>
                 </h1>
             </h1>
             <div className="card-container flex items-center justify-center h-2/3 w-screen">
                 <div className={`cards rounded border-solid border-2 border-slate-700 grid grid-cols-${val} bg-slate-100`}>
+                    
                     {
-                        [...Array((val*val))].map((items,i)=>{
+                        cards.map((items,i)=>{                            
                             return(
-                                <div className="flex items-center font-bold hover:bg-sky-700 m-2 justify-center h-16 w-16 rounded border-solid border-2 border-slate-700 bg-slate-100">{i+1}</div>
+                                <div key={i} className="flex items-center font-bold hover:bg-sky-700 m-2 justify-center h-16 w-16 rounded border-solid border-2 border-slate-700 bg-slate-100">{items.id}</div>
                             )
                         })
                     }
+
                     
                 </div>
             </div>
---
+++ src/Utils/Data.js
@@ -0,0 +1,18 @@
+const data=[
+    {
+        id:1,
+        status:false
+    },
+    {
+        id:5,
+        status:false
+    },
+    {
+        id:4,
+        status:false
+    },
+    {
+        id:1,
+        status:false
+    }
+]
